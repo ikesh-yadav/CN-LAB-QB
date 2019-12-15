404082b85f036004def89c0ef3ad347a3640208c
ikesh-yadav <ikeshyadav1@gmail.com>
2019-12-15 14:07:12 +0530

    2

--- Q1/client.c
+++ Q1/client.c
@@ -1,18 +1,18 @@
 #include<stdio.h>
-#include<arpha/inet.h>
+#include<winsock.h>//linux #include<arpha/inet.h>
 int main(){
 	int sock_fd;
 	char fname[50],buffer[1024];
 	struct sockaddr_in addr;
-	sock_addr=socket(AF_INET,SOCK_STREAM,0);
+	sock_fd=socket(AF_INET,SOCK_STREAM,0);
 	addr.sin_family=AF_INET;
 	addr.sin_addr.s_addr=inet_addr("127.0.0.1");
 	addr.sin_port=htons(7000);
-	connect(sock_addr,(struct sockaddr *)&addr, sizeof(addr));
+	connect(sock_fd,(struct sockaddr *)&addr, sizeof(addr));
 	printf("Enter filename:");
 	scanf("%s",fname);
 	send(sock_fd,fname,size(fname),0);
-	while(recv(sock_fd,buffer,size(buffer))){
+	while(recv(sock_fd,buffer,size(buffer),0)){
 		print("%s",buffer);
 	}
 	close(sock_fd);
--- Q1/server.c
+++ Q1/server.c
@@ -1,27 +1,27 @@
 #include<stdio.h>
-#include<arpha/inet.h>
+#include<Winsock2.h>//linux #include<arpha/inet.h>
 #include<fcntl.h>
 int main(){
 	int fd,sock_fd,new_sock_fd;
 	char fname[50],buffer[1024];
-	sruct sockaddr_in addr;
+	struct sockaddr_in addr;
 	sock_fd=socket(AF_INET,SOCK_STREAM,0);
 	addr.sin_family=AF_INET;
-	addr.sin_addr.s_addr=INET_ANY;
+	addr.sin_addr.s_addr=inet_addr("127.0.0.1");//SHould be INET_ANY
 	addr.sin_port=htons(7000);
 	bind(sock_fd,(struct sockaddr *) &addr,sizeof(addr));
 	printf("Server Online");
-	fflush();
+	fflush(stdin);
 	listen(sock_fd,5);
-	while(true){
-		new_sock_fd=accept(sock_fd,null,null);
-		recv(new_sock_fd,fname,size(fname),0);
+	while(1){
+		new_sock_fd=accept(sock_fd,NULL,NULL);
+		recv(new_sock_fd,fname,sizeof(fname),0);
 		fd=open(fname,O_RDONLY);
 		if(fd<0){
 			send(new_sock_fd,"file not found\n",15,0);
 		}else{
 			while(read(fd,buffer,size(buffer))){	
-				send(new_sock_fd,buffer,size(buffer));
+				send(new_sock_fd,buffer,size(buffer),0);
 			}
 		}
 		close(fd);
