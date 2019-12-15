1b8662a0996fd72598e1a030157ec49030345e14
ikesh-yadav <ikeshyadav1@gmail.com>
2019-12-15 14:17:25 +0530

    not working in windows

--- Q1/client.c
+++ Q1/client.c
@@ -7,13 +7,13 @@ int main(){
 	sock_fd=socket(AF_INET,SOCK_STREAM,0);
 	addr.sin_family=AF_INET;
 	addr.sin_addr.s_addr=inet_addr("127.0.0.1");
-	addr.sin_port=htons(7000);
+	addr.sin_port=htons(7891);
 	connect(sock_fd,(struct sockaddr *)&addr, sizeof(addr));
 	printf("Enter filename:");
 	scanf("%s",fname);
-	send(sock_fd,fname,size(fname),0);
-	while(recv(sock_fd,buffer,size(buffer),0)){
-		print("%s",buffer);
+	send(sock_fd,fname,sizeof(fname),0);
+	while(recv(sock_fd,buffer,sizeof(buffer),0)){
+		printf("%s",buffer);
 	}
 	close(sock_fd);
 }
--- Q1/server.c
+++ Q1/server.c
@@ -8,23 +8,27 @@ int main(){
 	sock_fd=socket(AF_INET,SOCK_STREAM,0);
 	addr.sin_family=AF_INET;
 	addr.sin_addr.s_addr=inet_addr("127.0.0.1");//SHould be INET_ANY
-	addr.sin_port=htons(7000);
+	addr.sin_port=htons(7891);
 	bind(sock_fd,(struct sockaddr *) &addr,sizeof(addr));
 	printf("Server Online");
 	fflush(stdin);
 	listen(sock_fd,5);
 	while(1){
 		new_sock_fd=accept(sock_fd,NULL,NULL);
+		printf("ACCEPTING CONNECTION");
 		recv(new_sock_fd,fname,sizeof(fname),0);
+		printf("Client requesting:%s\n",fname);
 		fd=open(fname,O_RDONLY);
 		if(fd<0){
 			send(new_sock_fd,"file not found\n",15,0);
 		}else{
-			while(read(fd,buffer,size(buffer))){	
-				send(new_sock_fd,buffer,size(buffer),0);
+			printf("STARTING TRANSFER\n");
+			while(read(fd,buffer,sizeof(buffer))){	
+				send(new_sock_fd,buffer,sizeof(buffer),0);
 			}
 		}
 		close(fd);
 		close(new_sock_fd);
+		printf("SOCKET CLOSED\n");
 	}
 }
